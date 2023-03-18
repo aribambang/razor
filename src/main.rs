a7041d156c1d973bdb6f8363119e62399fb5554a
Ari Bambang Kurniawan <aribk210@gmail.com>
2023-03-18 11:04:55 +0700

    feat: 🎸 health checker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::middleware::Logger;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde_json::json;
+
+#[get("/api/health")]
+async fn health_handler() -> impl Responder {
+    const MESSAGE: &str = "Razor API";
+
+    HttpResponse::Ok().json(json!({"status": "success", "message": MESSAGE}))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
+
+    println!("🚀 Server started successfully");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(health_handler)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
